776d9b1be9dbb650adbe131b1cdce412680e73b0
meggle <meggani@gmail.com>
2017-03-12 21:28:40 -0400

    oh god how did this get here
    i am not good with computer

---
+++ Gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
